eeb00ce9d2d83826e6f911fd8ca8a0df750f82ae
Nikola Milakovic <nikola.milakovc08@gmail.com>
2026-07-16 21:10:17 +0200

    Update project files

--- lesson1/test_sql.py
+++ lesson1/test_sql.py
@@ -8,48 +8,48 @@ import crud
 class StudentDatabaseTests(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.TemporaryDirectory()
-        self.original_db_name = sql.DB_NAME
-        sql.DB_NAME = os.path.join(self.temp_dir.name, "test_school.db")
-        sql.create_table()
+        self.original_db_name = crud.DB_NAME
+        crud.DB_NAME = os.path.join(self.temp_dir.name, "test_school.db")
+        crud.create_table()
 
     def tearDown(self):
-        sql.DB_NAME = self.original_db_name
+        crud.DB_NAME = self.original_db_name
         self.temp_dir.cleanup()
 
     def test_add_and_get_student(self):
-        sql.add_student("Amina", 17)
+        crud.add_student("Amina", 17)
 
-        students = sql.get_all_students()
+        students = crud.get_all_students()
 
         self.assertEqual(len(students), 1)
         self.assertEqual(students[0][1], "Amina")
         self.assertEqual(students[0][2], 17)
 
     def test_get_student_by_id(self):
-        sql.add_student("Marko", 18)
-        student_id = sql.get_all_students()[0][0]
+        crud.add_student("Marko", 18)
+        student_id = crud.get_all_students()[0][0]
 
-        student = sql.get_student_by_id(student_id)
+        student = crud.get_student_by_id(student_id)
 
         self.assertEqual(student, (student_id, "Marko", 18))
 
     def test_update_student_age(self):
-        sql.add_student("Sara", 16)
-        student_id = sql.get_all_students()[0][0]
+        crud.add_student("Sara", 16)
+        student_id = crud.get_all_students()[0][0]
 
-        sql.update_student_age(student_id, 17)
+        crud.update_student_age(student_id, 17)
 
-        student = sql.get_student_by_id(student_id)
+        student = crud.get_student_by_id(student_id)
         self.assertEqual(student[2], 17)
 
     def test_delete_student(self):
-        sql.add_student("Edis", 19)
-        student_id = sql.get_all_students()[0][0]
+        crud.add_student("Edis", 19)
+        student_id = crud.get_all_students()[0][0]
 
-        sql.delete_student(student_id)
+        crud.delete_student(student_id)
 
-        self.assertEqual(sql.get_all_students(), [])
-        self.assertIsNone(sql.get_student_by_id(student_id))
+        self.assertEqual(crud.get_all_students(), [])
+        self.assertIsNone(crud.get_student_by_id(student_id))
 
 
 if __name__ == "__main__":
